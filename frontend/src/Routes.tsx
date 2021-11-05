87e78b20c6eab5a13e8bdae72e84a00e05e683f3
paulovarao <paulovarao@gmail.com>
2021-11-05 11:40:20 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,16 @@
+import Dashboard from "pages/Dashboard";
+import Home from "pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const Routess = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default Routess;
